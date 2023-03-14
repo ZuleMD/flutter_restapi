fa0819fee43af6b34ad29ae6c75c84c16ea5bc51
mariem <ameny.ferchichi99@gmail.com>
2023-03-14 22:06:13 +0100

    :)

---
+++ lib/caseslist.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_restapi/models/cases.dart';
+import 'package:flutter_restapi/detailwidget.dart';
+
+class CasesList extends StatelessWidget {
+  final List<Cases> cases;
+  CasesList({required Key key, required this.cases}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: cases == null ? 0 : cases.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+              child: InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => DetailWidget(cases[index])),
+              );
+            },
+            child: ListTile(
+              leading: Icon(Icons.person),
+              title: Text(cases[index].name),
+              subtitle: Text(cases[index].age.toString()),
+            ),
+          ));
+        });
+  }
+}
